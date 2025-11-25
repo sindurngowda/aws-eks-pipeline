45c55eaa9116efeb4c95d39b0b86238dc886f8fe
sindurngowda <sindurngowda@gmail.com>
2025-11-26 01:53:02 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,20 +16,3 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "aws_access_key" {
-  description = "AWS Access Key ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_account_id" {
-  description = "AWS Account ID"
-  type        = string
-}
-
