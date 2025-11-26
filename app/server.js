b9c2c8ac3c3bdcead060622d8bfc533ff83dba71
sindurngowda <sindurngowda@gmail.com>
2025-11-26 13:35:27 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World from EKS! Deployed via Jenkins & Helm.\n');
+    res.end('DevOps Assessment Completed.\n');
 });
 
 server.listen(port, () => {
