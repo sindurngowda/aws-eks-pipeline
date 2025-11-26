886c480bc49a496150d48eae8ef4cc622f579542
sindurngowda <sindurngowda@gmail.com>
2025-11-26 12:16:25 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,4 @@
-terraform {
-  required_version = "1.14.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.97.0"
-    }
-  }
+provider "aws" {
+  region = var.region
 }
 
